ecf8367ed821507fbd4112a5377417f98a36e20c
WeerwaarT <lacking.style@outlook.com>
2024-08-18 02:21:53 +0200

    DP practice

--- problems/740.py
+++ problems/740.py
@@ -1,16 +1,46 @@
 # 740. 删除并获得点数
 # https://leetcode.cn/problems/delete-and-earn/
+import collections
 from typing import List
 
 
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        d = {}
+        counter = collections.defaultdict(int)
         for num in nums:
-            d[num] = d.get(num, 0) + num
+            counter[num] += num
 
-        p0, p1 = 0, 0
-        for key in sorted(d.keys()):
-            p0, p1 = p1, max(p1, p0 + d[key])
+        keys = sorted(counter.keys())
+        max_points = 0
+        vals = [counter[keys[0]]]
 
-        return max(p0, p1)
+        def rob() -> None:
+            nonlocal max_points
+            m = len(vals)
+            if m == 1:
+                max_points += vals[0]
+                return
+
+            p0, p1 = vals[0], 0
+            for j in range(1, m):
+                p0, p1 = max(p0, p1 + vals[j]), p0
+
+            max_points += p0
+
+        n = len(keys)
+        for i in range(1, n):
+            if keys[i] == keys[i - 1] + 1:
+                vals.append(counter[keys[i]])
+            else:
+                rob()
+                vals = [counter[keys[i]]]
+
+        rob()
+        return max_points
+
+
+if __name__ == '__main__':
+    print(Solution().deleteAndEarn([1, 2, 3]))
+    print(Solution().deleteAndEarn([3,4,2]))
+    print(Solution().deleteAndEarn([2,2,3,3,3,4]))
+    print(Solution().deleteAndEarn([1,1,1,2,4,5,5,5,6]))
